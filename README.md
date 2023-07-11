45317a4f7016a958f32b27f9ab1540ce78739517
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-07-11 12:30:21 +0100

    adding middleware. updating README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Getting up and running with node and express.  
 
-https://www.digitalocean.com/community/tutorials/nodejs-express-basics
+https://www.digitalocean.com/community/tutorials/nodejs-express-basics  
+
+Start the server via:  
+$> node server.js
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 // Grab the express module
-const express = require('express');
+const express = require("express");
 
 // The module is a function, run this function to create our app
 const app = express();
 
+// Simple middleware function to print the time in the console during
+// every request
+app.use((req, res, next) => {
+    console.log("Time: ", Date.now());
+    next();
+});
+
 // Handle get requests
-app.get('/', (req, res) => {
-  res.send('Successful response.');
+app.get("/", (req, res) => {
+    res.send("Successful response.");
 });
 
 // Server to listen on port 3000
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(3000, () => console.log("Example app is listening on port 3000."));
