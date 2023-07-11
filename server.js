3e362aa385ca712c644674f1c9d63e4938007129
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-07-11 12:47:07 +0100

    middleware to serve a webpage

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use((req, res, next) => {
     next();
 });
 
+// Adding path to middleware, function will only rn for requests sent to
+// 127.0.0.1:3000/request-type
+app.use("/request-type", (req, res, next) => {
+    console.log("Request type: ", req.method);
+    next();
+});
+
+// Built in middleware for serving static files
+app.use("/public", express.static("public"));
+
 // Handle get requests
 app.get("/", (req, res) => {
     res.send("Successful response.");
